95033984f8e4c54ef1feb2034f01df18ba375841
Pablo Henrique <pablohenriqueadm94@gmail.com>
2024-11-02 09:33:54 -0300

    att

---
+++ projeto-figmaland/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-blue-500'>Hello world</h1>
+    <h2 className='text-red-600'>caralho</h2>
+    </>
+  )
+}
+
+export default App
